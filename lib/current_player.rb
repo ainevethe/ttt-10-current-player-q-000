ecb43f90ff40eff296d5b7eb63de370420d50dbc
Crista Cooper <aine3d@gmail.com>
2017-10-14 22:30:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 
 def turn_count(board)
-
+board = [" "," "," "," "," "," "," "," "," "]
   counter = 0
   while counter < 9
-    [board].each do
+    board.each do
       counter += 1
     end
   end
